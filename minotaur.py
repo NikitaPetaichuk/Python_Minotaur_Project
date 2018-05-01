857dcc6b3015ca6eac09a63df7e10fab8a68188e
NikitaPetaichuk <pet.ai.4.uk@yandex.ru>
2018-05-01 11:27:06 +0300

    Version 1.21

--- minotaur.py
+++ minotaur.py
@@ -7,7 +7,6 @@ def write_in_file(path_to_minor):
         for elem in path_to_minor:
              output.write(elem + "\n")
 
-
 def search_file(start_dir, file_name):
     for root, dirs, files in os.walk(start_dir):
         for f in files:
@@ -28,10 +27,11 @@ def search_Minotaur(start, file_name, chain):
                     chain.pop()
                     return 0
                 for way in lines:
-                    if way.split()[0] == '@include' and search_Minotaur(start, way.split()[1], chain) == 1:
-                        return 1
+                    if way.split()[0] == '@include' and way.split()[1].endsWith(".txt"):
+                        if search_Minotaur(start, way.split()[1], chain) == 1:
+                            return 1
             except IndexError:
-                print("Incorrect file data!")
+                print("Incorrect file data in " + now_path + "!")
                 return 0
     chain.pop()
     return 0
